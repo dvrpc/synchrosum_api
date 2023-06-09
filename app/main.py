ed54f7f3c2a9ea874b21d6b3bb3f8c0cdf2cdfd8
Mark Morley <mmorley0395@gmail.com>
2023-06-09 12:52:31 -0400

    percent sign removed from path which threw 405 error

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
-import os
-import shutil
-from fastapi import FastAPI, UploadFile, BackgroundTasks
+# import os
+# import shutil
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile, BackgroundTasks
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from plan_belt import synchro_summarizer
+# from plan_belt import synchro_summarizer
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
 
 
 try:
@@ -17,17 +17,35 @@ except ImportError:
 
 @app.post(f"{URL_PATH}/uploadfiles/")
 async def create_upload_files(
-    files: list[UploadFile], background_tasks: BackgroundTasks
+    files: Annotated[
+        list[UploadFile], File(description="Multiple files as UploadFile")
+    ],
 ):
-    for file in files:
-        with open(file.filename, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
-    cwd = os.getcwd()
-    summary = cwd
-    summary_filepath = os.path.normpath(summary[0])
-    summary_filename = str(os.path.basename(os.path.normpath(summary[0])))
-    return FileResponse(
-        summary_filepath,
-        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        filename=summary_filename,
-    )
+    return {"filenames": [file.filename for file in files]}
+
+# @app.post(f"{URL_PATH}/uploadfiles/")
+# async def create_upload_files(
+#     files: list[UploadFile], background_tasks: BackgroundTasks
+# ):
+#     for file in files:
+#         return {"filename": file.filename}
+    # with open(file.filename, "wb") as buffer:
+    # shutil.copyfileobj(file.file, buffer)
+    # cwd = os.getcwd()
+    # summary = cwd
+    # summary_filepath = os.path.normpath(summary[0])
+    # summary_filename = str(os.path.basename(os.path.normpath(summary[0])))
+    # return FileResponse(
+    # summary_filepath,
+    # media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    # filename=summary_filename,
+    # )
+
+
+app.mount("/", StaticFiles(directory="app/static",
+                           html=True), name="static")
+
+
+@app.get("/")
+async def read_index():
+    return HTMLResponse('app/static/index.html')
