bff8c2ee5d261eb37b2a1700565c9a9362bb47d9
Mark Morley <mmorley0395@gmail.com>
2023-06-09 14:42:54 -0400

    returns from browser successfully, now need to add sim handler

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-# import os
-# import shutil
+import os
+import shutil
 from typing import Annotated
 from fastapi import FastAPI, File, UploadFile, BackgroundTasks
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
-# from plan_belt import synchro_summarizer
+from plan_belt.synchro_summarizer import SynchroTxt
 
 app = FastAPI()
 
@@ -17,29 +17,19 @@ except ImportError:
 
 @app.post(f"{URL_PATH}/uploadfiles/")
 async def create_upload_files(
-    files: Annotated[
-        list[UploadFile], File(description="Multiple files as UploadFile")
-    ],
+    files: list[UploadFile], background_tasks: BackgroundTasks
 ):
-    return {"filenames": [file.filename for file in files]}
-
-# @app.post(f"{URL_PATH}/uploadfiles/")
-# async def create_upload_files(
-#     files: list[UploadFile], background_tasks: BackgroundTasks
-# ):
-#     for file in files:
-#         return {"filename": file.filename}
-    # with open(file.filename, "wb") as buffer:
-    # shutil.copyfileobj(file.file, buffer)
-    # cwd = os.getcwd()
-    # summary = cwd
-    # summary_filepath = os.path.normpath(summary[0])
-    # summary_filename = str(os.path.basename(os.path.normpath(summary[0])))
-    # return FileResponse(
-    # summary_filepath,
-    # media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    # filename=summary_filename,
-    # )
+    for file in files:
+        with open(file.filename, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+            cwd = os.getcwd()
+            summary = SynchroTxt(cwd + "/" + file.filename)
+            filepath = summary.excel_path
+    return FileResponse(
+        filepath,
+        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        filename="synchro_sum.xlsx",
+    )
 
 
 app.mount("/", StaticFiles(directory="app/static",
