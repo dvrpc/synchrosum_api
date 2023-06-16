e7556a8ea3a4112400c9a0713f65c5fb9819b474
Kris Warner <kdwarn@protonmail.com>
2023-06-16 12:05:12 -0400

    Add path
    Some black formatting was applied as well.

--- app/main.py
+++ app/main.py
@@ -16,9 +16,7 @@ except ImportError:
 
 
 @app.post(f"{URL_PATH}/uploadfiles/")
-async def create_upload_files(
-    files: list[UploadFile], background_tasks: BackgroundTasks
-):
+async def create_upload_files(files: list[UploadFile], background_tasks: BackgroundTasks):
     files.sort(key=lambda f: os.path.splitext(f.filename)[1])
     for file in files:
         with open(file.filename, "wb") as buffer:
@@ -52,10 +50,9 @@ def cleanup():
                 os.remove(file)
 
 
-app.mount("/", StaticFiles(directory="app/static",
-                           html=True), name="static")
+app.mount(f"{URL_PATH}/", StaticFiles(directory="app/static", html=True), name="static")
 
 
-@app.get("/")
+@app.get(f"{URL_PATH}/")
 async def read_index():
-    return HTMLResponse('app/static/index.html')
+    return HTMLResponse("app/static/index.html")
